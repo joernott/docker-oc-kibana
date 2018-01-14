73b3732feddba4dc4262a0c9ed607fdd282cc70c
Jörn Ott <joern.ott@ott-consult.de>
2018-01-14 23:00:36 +0100

    Add label, track image history

--- src/tmp/install/oc-kibana.sh
+++ src/tmp/install/oc-kibana.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-curl -sSo /tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
 source /tmp/install/functions.sh
 add_repos elasticsearch${KIBANA_VERSION:0:1}
 create_user_and_group
